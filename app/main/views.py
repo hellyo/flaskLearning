c4e352469757d6649ce68daea4e741c01db5f282
zac <zacmyx@gmail.com>
2018-01-24 22:56:43 +0800

    page 123

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import render_template,session,redirect,url_for,abort,flash
+from flask import render_template,session,redirect,url_for,abort,flash,request,current_app
 
 from .forms import EditProfileForm,EditProfileAdminForm,PostForm
 from . import main
@@ -17,11 +17,17 @@ def index():
         post = Post(body=form.body.data,author=current_user._get_current_object())
         db.session.add(post)
         return redirect(url_for('.index'))
+    
+    page = request.args.get('page',1,type=int)
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page,per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+    error_out=False)
     try:
-        posts = Post.query.order_by(Post.timestamp.desc()).all()
+        posts = pagination.items
     except:
         posts = None
-    return render_template('index.html',form=form,posts=posts)
+    print(pagination.total)
+    return render_template('index.html',form=form,posts=posts,pagination=pagination)
 
 #    return render_template('index.html',name=session.get('name'),known=session.get('known',False))
 
--- app/models.py
+++ app/models.py
@@ -171,6 +171,32 @@ class User(UserMixin,db.Model):
 		rtnStr= '{url}/{hash}?s={size}&d={default}&r={rating}'.format(url=url,hash=hash,size=size,default=default,rating=rating)
 		#print(rtnStr)
 		return rtnStr
+	@staticmethod
+	def generate_fake(count = 100):
+		from sqlalchemy.exc import IntegrityError
+		from random import seed
+		import forgery_py
+
+		seed()
+		for i in range(count):
+			u = User(email=forgery_py.internet.email_address(),
+			username=forgery_py.internet.user_name(True),
+			password=forgery_py.lorem_ipsum.word(),
+			confirmed=True,
+			name=forgery_py.name.full_name(),
+			location=forgery_py.address.city(),
+			about_me=forgery_py.lorem_ipsum.sentence(),
+			member_since=forgery_py.forgery.date.date(True))
+			db.session.add(u)
+			try:
+				db.session.commit()
+			except IntegrityError:
+				db.session.rollback()
+
+
+
+
+    		
 
 		
 
@@ -193,4 +219,21 @@ class Post(db.Model):
 	body = db.Column(db.Text)
 	timestamp = db.Column(db.DateTime,index=True,default=datetime.utcnow())
 	author_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+
+	@staticmethod
+	def generate_fake(count = 100):
+		from random import seed,randint
+		import forgery_py
+
+		seed()
+		user_count = User.query.count()
+		for i in range(count):
+			u = User.query.offset(randint(0,user_count -1)).first()
+			p = Post(body=forgery_py.lorem_ipsum.sentences(randint(1,3)),
+			timestamp=forgery_py.date.date(True),
+			author=u)
+		db.session.add(p)
+		db.session.commit()
+		print("fake data generated!")
+    		
 	
