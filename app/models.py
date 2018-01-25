9cd1545c676e0b2aab89528e83ba416623f6e6d1
zac <zacmyx@gmail.com>
2018-01-26 03:22:56 +0800

    chapter 11 end

--- app/models.py
+++ app/models.py
@@ -10,6 +10,8 @@ from flask import current_app,request
 from . import db
 from datetime import datetime
 import hashlib
+import bleach
+from markdown import markdown
 
 
 class Permission:
@@ -217,6 +219,7 @@ class Post(db.Model):
 	
 	id = db.Column(db.Integer,primary_key=True)
 	body = db.Column(db.Text)
+	body_html = db.Column(db.Text)
 	timestamp = db.Column(db.DateTime,index=True,default=datetime.utcnow())
 	author_id = db.Column(db.Integer,db.ForeignKey('users.id'))
 
@@ -235,5 +238,13 @@ class Post(db.Model):
 		db.session.add(p)
 		db.session.commit()
 		print("fake data generated!")
+
+	@staticmethod
+	def on_changed_body(target,value,oldvalue,initiator):
+		allowed_tags = ['a','abbr','acronym','b','blockquote','code','em','i','li','ol','pre','strong','ul','h1','h2','h3','p']
+		target.body_html = bleach.linkify(bleach.clean(
+		markdown(value,output_format='html'),
+		tags = allowed_tags,strip=True))
+db.event.listen(Post.body,'set',Post.on_changed_body)
     		
 	
